382b91a2e4c717251bba150f3dafa6d7aa9dbce3
Acurasa <unxetra@gmail.com>
2023-10-13 09:24:31 +0200

    init commit

---
+++ Figure.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using FigureModel.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Figure.DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) 
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Gaming Accessories", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Posters", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Clothes", DisplayOrder = 3 });
+        }
+    }
+}
---
+++ Figure.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figure.DataAccess.Repository.IRepository
+{
+    internal interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T Get(Expression<Func<T, bool>> filter);
+
+        void Add(T entity);
+        void Remove(T entity);
+        void Delete(T entity);
+
+        void RemoveRange (IEnumerable<T> entity); 
+       
+    }
+}
